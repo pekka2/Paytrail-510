66ab572cae2ff05b310276efce2f5105131c67e3
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-12-19 13:14:55 +0200

    Add files via upload

--- upload/Opencart-1.5.6/catalog/controller/checkout/failure.php
+++ upload/Opencart-1.5.6/catalog/controller/checkout/failure.php
@@ -1,1 +1,56 @@
-hh
+<?php
+class ControllerCheckoutFailure extends Controller {
+	public function index() {
+		$this->load->language('checkout/failure');
+
+		$this->document->setTitle($this->language->get('heading_title'));
+
+		$this->data['breadcrumbs'] = array();
+
+		$this->data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_home'),
+			'href' => $this->url->link('common/home')
+		);
+
+		$this->data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_basket'),
+			'href' => $this->url->link('checkout/cart')
+		);
+
+		$this->data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_checkout'),
+			'href' => $this->url->link('checkout/checkout', '', true)
+		);
+
+		$this->data['breadcrumbs'][] = array(
+			'text' => $this->language->get('text_failure'),
+			'href' => $this->url->link('checkout/failure')
+		);
+
+		$this->data['heading_title'] = $this->language->get('heading_title');
+
+		$this->data['text_message'] = sprintf($this->language->get('text_message'), $this->url->link('information/contact'));
+
+		$this->data['button_continue'] = $this->language->get('button_continue');
+
+		$this->data['continue'] = $this->url->link('common/home');
+
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/success.tpl')) {
+			$this->template = $this->config->get('config_template') . '/template/common/success.tpl';
+		} else {
+			$this->template = 'default/template/common/success.tpl';
+		}
+
+		$this->children = array(
+			'common/column_left',
+			'common/column_right',
+			'common/content_top',
+			'common/content_bottom',
+			'common/footer',
+			'common/header'	
+		);
+
+		$this->response->setOutput($this->render());
+
+	}
+}
