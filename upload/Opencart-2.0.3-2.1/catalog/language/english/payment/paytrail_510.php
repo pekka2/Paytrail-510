6a93fd29d613179f7c12ab7805d46209b5d22599
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-12-19 16:38:08 +0200

    Add files via upload

--- upload/Opencart-2.0.3-2.1/catalog/language/english/payment/paytrail_510.php
+++ upload/Opencart-2.0.3-2.1/catalog/language/english/payment/paytrail_510.php
@@ -1,1 +1,16 @@
+<?php
+// Text
+$_['text_title']            = 'Web payment';
+$_['text_paid_success']     = 'Payment has been processed successfully! payer’s bank: ';
+$_['text_paid_cancel']      = 'Payment process has been cancelled';
 
+// Button
+$_['button_back']           = 'Cancel';
+
+// Help
+$_['help_banks']            = 'You can pay directly through your web bank';
+$_['help_credit']           = 'You can pay with the debit or credit card of your choice';
+
+// Error
+$_['error_order_totals']     = '<b>VIRHE!</b> Verkkokaupan laskema tuotteiden ja muiden kulujen yhteishinta kassalla on pienempi kuin ostoksen sekä toimitus- ja muiden kulujen todellinen yhteishinta!';
+?>
