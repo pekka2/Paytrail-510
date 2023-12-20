059f9890f9db02149f2dea6b8ff8bfbc1f6b368f
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-12-20 10:14:22 +0200

    Add files via upload

---
+++ upload/Opencart-2.2/admin/language/fi-fi/payment/paytrail_510.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']          = 'Paytrail 5.10';
+
+// Text 
+$_['text_payment']           = 'Maksutavat';
+$_['text_success']           = 'Maksutavan Paytrail 5.10 muokkaaminen suoritettu!';
+$_['text_paytrail_510']      = '<a target="_BLANK" href="https://www.paytrail.com/"><img src="view/image/payment/paytrail.png" alt="Paytrail  verkkosivusto" title="Paytrail verkkosivusto" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_clear_success']     = 'Lokitietojen tyhjennys onnistui!';
+$_['text_edit']              = 'Muokkaa Paytrailia';
+$_['text_extension']         = 'Laajennukset';
+      
+// Entry
+$_['entry_merchant']            = 'Myyjän tunniste';
+$_['entry_security']            = 'Turva-avain';
+$_['entry_order_status']        = 'Tilauksen tila';
+$_['entry_order_cancel_status'] = 'Epäonnistuneen tilauksen tila:';
+$_['entry_geo_zone']            = 'Maantieteellinen alue:';
+$_['entry_status']              = 'Tila';
+$_['entry_sort_order']          = 'Järjestys';
+$_['entry_sandbox']             = 'Testitila';
+$_['entry_paytrail_font_size']  = 'Tekstin \'Verkkokauppa\' koko pikseleinä';
+$_['entry_log']                 = 'Loki';
+$_['entry_failed_log']          = 'Loki 2';
+$_['entry_store']               = 'Kauppa';
+
+// Error
+$_['error_permission']   = 'Varoitus: Sinulla ei ole oikeuksia maksutavan Paytrail muokkaamiseen!';
+$_['error_merchant']     = 'Myyjän tunniste (MERCHANT) on pakollinen tieto!';
+$_['error_security']     = 'Turva-avain on pakollinen tieto!';
+
+// Help
+$_['help_merchant']      = 'Myyjän tunniste eli Merchant ID, jonka olet saanut Paytraililta.';
+$_['help_security']      = 'Turva-avain eli Secret Key, jonka olet saanut Paytraililta.';
+$_['help_order_status']  = 'Onnistuneen tilauksen tila, kun asiakas palaa maksamisen jälkeen pankin sivulta kauppaan.';
+$_['help_cancel_status'] = 'Epäonnistuneen tilauksen tila, kun asiakas palaa pankin sivulta kauppaan (joko peruutettuaan itse maksamisen tai maksaminen on muusta syystä keskeytynyt).';
+$_['help_geo_zone']      = 'Valitse tähän se alue, jonka alueella verkkokaupassasi on myyntiä.';
+$_['help_sandbox']       = 'Voit tällä testata tämän laajennuksen toimintoja. Kun käytät tätä testitilaa, \'Myyjän tunniste\' ja \'Turva-avain\' eivät ole pakollisia tietoja.';
+$_['help_font_size']     = 'Tekstin \'Verkkokauppa\' koko pikseleissä (syötä vain numeroita!)';
+$_['help_log']          = 'Loki-tiedosto, joka sisätää kaiken liikenteen, onnistuneet ja epäonnistuneet maksutapahtumat.';
+$_['help_failed_log']   = 'Loki-tiedoston, joka sisätää jostakin syystä epäonnistuneet tai asiakkaan omatoimisesti peruuttamat maksutapahtumat.';
+?>
