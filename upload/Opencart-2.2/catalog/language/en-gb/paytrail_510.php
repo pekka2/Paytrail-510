78537b0a89b217d4a73c3df3c694b90bd755e103
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-12-20 10:22:17 +0200

    Add files via upload

---
+++ upload/Opencart-2.2/catalog/language/en-gb/paytrail_510.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_title']            = 'Web payment';
+$_['text_paid_success']     = 'Payment has been processed successfully! payer’s bank: ';
+$_['text_paid_cancel']      = 'Payment process has been cancelled';
+
+// Button
+$_['button_back']           = 'Cancel';
+
+// Help
+$_['help_banks']            = 'You can pay directly through your web bank';
+$_['help_credit']           = 'You can pay with the debit or credit card of your choice';
+
+// Error
+$_['error_order_totals']     = '<b> ERROR! </b> The total cost of products and other costs at checkout calculated by the online store is less than the actual total price of the purchase and delivery and other costs!';
+?>
