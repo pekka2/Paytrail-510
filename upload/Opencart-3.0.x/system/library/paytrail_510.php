4f1dd0dbe90eabe32c08fa38e9f82c3329e27d38
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-12-20 17:00:08 +0200

    Add files via upload

---
+++ upload/Opencart-3.0.x/system/library/paytrail_510.php
@@ -0,0 +1,80 @@
+<?php
+class Paytrail_510 {
+
+	public function __construct($checkout_merchant, $checkout_secret, $headers, $body){
+
+        $this->checkout_merchant = $checkout_merchant;
+        $this->checkout_secret = $checkout_secret;
+        $this->headers = $headers;
+        $this->body = $body;
+        $this->cof_request_id = null;
+
+	}
+
+	public function send(){
+		    require_once(DIR_SYSTEM . 'pm-extension/vendor/autoload.php');
+
+            $client = new \GuzzleHttp\Client(array('headers' => $this->headers));
+            $response = null;
+            try {
+            	$response = $client->post('https://services.paytrail.com/payments', array('body' => $this->body));
+            } catch (\GuzzleHttp\Exception\ClientException $e) {
+               if ($e->hasResponse()) {
+                  $response = $e->getResponse();
+                  echo "Unexpected HTTP status code: {$response->getStatusCode()}\n\n";  $log = new Log('paytrail-510.log');
+
+               $string =  "Unexpected HTTP status code: {$response->getStatusCode()}";
+               $logs = new Log('paytrail_error.log');
+               $logs->write($string);
+              }
+           }
+
+           $data['response'] = '';
+
+           $responseBody = $response->getBody()->getContents();
+              // Flatten Guzzle response headers
+              $responseHeaders = array_column(array_map(function ($key, $value) {
+                return [ $key, $value[0] ];
+             }, array_keys($response->getHeaders()), array_values($response->getHeaders())), 1, 0);
+             
+           $responseHmac = $this->calculateHmac($responseHeaders,$responseBody);
+
+           if (!isset($response->getHeader('signature')[0]) || isset( $response->getHeader('signature')[0]) && $responseHmac !== $response->getHeader('signature')[0]) {
+             echo "<pre>";
+             echo "   Response HMAC signature mismatch!";
+             echo "</pre>";
+
+               $string =  "Response HMAC signature mismatch!";
+               $logs = new Log('paytrail_error.log');
+               $logs->write($string);
+          } else {
+              $data['response'] =  json_encode(json_decode($responseBody), JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES);
+          }
+
+            $this->cof_request_id =  "\n\nRequest ID: {$response->getHeader('request-id')[0]}\n\n";
+
+          return $data['response'];
+	}
+    
+	protected function calculateHmac($params, $body){
+       $includedKeys = array_filter(array_keys($params), function ($key) {
+           return  preg_match('/^checkout-/', $key);
+       });
+        sort($includedKeys, SORT_STRING);
+        $hmacPayload = array_map(
+            function ($key) use ($params) {
+                return join(':', [ $key, $params[$key] ]);
+            },
+            $includedKeys
+        );
+
+        array_push($hmacPayload, $body);
+
+       return hash_hmac('sha256', join("\n", $hmacPayload), $this->checkout_secret);
+    }
+
+	public function getRequestId(){
+		return $this->cof_request_id;
+    }
+}
+?>
