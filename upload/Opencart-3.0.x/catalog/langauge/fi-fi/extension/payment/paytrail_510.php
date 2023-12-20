f97e8e2cad89433adb3f3ceb89eb7d6d640b940d
Pekka Mansikka <pekka2@users.noreply.github.com>
2023-12-20 16:51:25 +0200

    Add files via upload

---
+++ upload/Opencart-3.0.x/catalog/langauge/fi-fi/extension/payment/paytrail_510.php
@@ -0,0 +1,16 @@
+<?php
+// Text
+$_['text_title']            = 'Verkkomaksu';
+$_['text_paid_success']     = 'Maksu on suoritettu onnistuneesti! Maksajan pankki: ';
+$_['text_paid_cancel']      = 'Maksun suorittaminen on keskeytetty';
+
+// Button
+$_['button_back']           = 'Peruuta';
+
+// Help
+$_['help_banks']            = 'Voit maksaa suoraan oman verkkopankkisi kautta';
+$_['help_credit']           = 'Voit maksaa valitsemallasi pankki- tai luottokortilla';
+
+// Error
+$_['error_order_totals']     = '<b>VIRHE!</b> Verkkokaupan laskema tuotteiden ja muiden kulujen yhteishinta kassalla on pienempi kuin ostoksen sekä toimitus- ja muiden kulujen todellinen yhteishinta!';
+?>
