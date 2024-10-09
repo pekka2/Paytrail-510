98ad8a95dc2321ab9c2bbb41e385bc60394fd0cc
Pekka Mansikka <pekka2@users.noreply.github.com>
2024-10-09 11:49:11 +0300

    Update paytrail_510.php

--- upload/Opencart-2.0.3-2.1/catalog/controller/payment/paytrail_510.php
+++ upload/Opencart-2.0.3-2.1/catalog/controller/payment/paytrail_510.php
@@ -89,7 +89,7 @@ class ControllerPaymentPaytrail510 extends Controller {
                  $item[] = array(
                                       'unitPrice' => (int)round($order_info['total'] * 100,0),
                                       'units' => (int)1,
-                                      'vatPercentage' => (int)24,
+                                      'vatPercentage' => 25.5,
                                       'productCode' => '#'. $this->session->data['order_id'],
                                       'deliveryDate' => date('Y-m-d')
                  );
